2e797396bf3b68d28bbf2c331d7ee8eb7b8d088a
EzequielG <ezequiel.guaymas07@gmail.com>
2023-05-11 10:43:17 -0300

    Se separo la actividad 1 de la clase 10

---
+++ clase10/actividad1/src/app.js
@@ -0,0 +1,24 @@
+//*Utilizar la misma estructura mostrada por el profesor, para poder levantar un servidor que utilice handlebars como motor de plantillas.
+import express from "express";
+import { __dirname } from "./utils.js";
+import handlebars from "express-handlebars";
+import viewsRouter from "./routes/views.router.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/static", express.static(__dirname + "/public"));
+
+//*configuracion motor plantilla HANDLEBARS
+app.engine("handlebars", handlebars.engine()); //? solo aqui
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+
+app.use("/views", viewsRouter);
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Escuchando al puerto ${PORT} en act1`);
+});
---
+++ clase10/actividad1/src/routes/views.router.js
@@ -0,0 +1,59 @@
+import { Router } from "express";
+
+const router = Router();
+
+//*Crear un array “users” que cuente con 5 usuarios de tipo objeto, cada uno con los datos mencionados arriba.
+const users = [
+  {
+    nombre: "Pablo",
+    apellido: "Militello",
+    edad: 40,
+    correo: "pmilitello@mail.com",
+    telefono: 65498713,
+  },
+  {
+    nombre: "Leandro",
+    apellido: "Lucci",
+    edad: 35,
+    correo: "llucci@mail.com",
+    telefono: 1013648,
+  },
+  {
+    nombre: "Carlos",
+    apellido: "Hermida",
+    edad: 50,
+    correo: "chermida@mail.com",
+    telefono: 132753129,
+  },
+  {
+    nombre: "Caro",
+    apellido: "Dominguez",
+    edad: 28,
+    correo: "cdominguez@mail.com",
+    telefono: 137852,
+  },
+  {
+    nombre: "Edgardo",
+    apellido: "Aranda",
+    edad: 63,
+    correo: "earanda@mail.com",
+    telefono: 345367,
+  },
+];
+
+//*Al llamar al método get ‘/’, generar un número random para elegir a alguno de los usuarios y mostrar el usuario seleccionado al azar en la plantilla.
+router.get("/", (req, res) => {
+  const indice = Math.floor(Math.random() * 5);
+  const usuario = users[indice];
+  res.render("datosPersonales", { ...usuario });
+});
+
+//*Observar los diferentes resultados en el navegador.
+//http://localhost:8080/views
+
+//? ejemplo para ver un listado completo de los usuarios (ir al handlebars)
+router.get("/listadoCompleto", (req, res) => {
+  res.render("listadoCompleto", { users });
+});
+
+export default router;
