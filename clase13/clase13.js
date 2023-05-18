93b84d5bb0c0761754a012c148ab02ed08b97e0d
EzequielG <ezequiel.guaymas07@gmail.com>
2023-05-18 16:44:34 -0300

    Se completo clase 13 - falta instalar Mongo en pcs

---
+++ clase13/clase13.js
@@ -0,0 +1,4 @@
+//-------------------------------------------CLASE 13 - MongoDB------------------------------------------------------
+//En este curso vemos base de datos de tipo NoSql
+// el profe recomienda ver base de datos SQL
+// a partir de la hora 1:00:00 empieza la instalacion del Mongodb
