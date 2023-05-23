3fedea9b72e749a687b6dd5b47c3be30b37a7a58
EzequielG <ezequiel.guaymas07@gmail.com>
2023-05-22 23:55:55 -0300

    clase 16

--- clase16/clase16.js
+++ clase16/clase16.js
@@ -1,1 +1,2 @@
 //-------------------------------------------CLASE 16 - Primera práctica integradora------------------------------------------------------
+//ver a partir de la hora 48:00 para ver mongo
