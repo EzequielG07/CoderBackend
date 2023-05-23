4fb1d32d7f5468d2489e8730cb38be0ac5f53d80
EzequielG <ezequiel.guaymas07@gmail.com>
2023-05-23 11:08:53 -0300

    Clase 16 completa con conexion a mongoDb

--- clase16/src/routes/students.router.js
+++ clase16/src/routes/students.router.js
@@ -1,19 +1,19 @@
-import { Router } from 'express'
-//import StudentsManager from '../Dao/studentsManagerFS.js'
-import StudentsManager from "../Dao/studentsManagerMongo.js";
+import { Router } from 'express';
+// import StudentsManager from '../Dao/stukdentsManagerFS.js';
+import StudentsManager from '../Dao/studentsManagerMongo.js';
 
-const studentsManager = new StudentsManager()
+const studentsManager = new StudentsManager();
 
-const router = Router()
+const router = Router();
 
 router.get('/', async (req, res) => {
-  const students = await studentsManager.getAllStudents()
-  res.json({ message: 'Students', students })
-})
+  const students = await studentsManager.getAllStudents();
+  res.json({ message: 'Students', students });
+});
 
 router.post('/', async (req, res) => {
-    const newStudent = await studentsManager.createStudent(req.body)
-    res.json({message:'Student created',student:newStudent})
-})
+  const newStudent = await studentsManager.createStudent(req.body);
+  res.json({ message: 'Student created', student: newStudent });
+});
 
-export default router
+export default router;
