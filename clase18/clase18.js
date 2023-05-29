aec789b7712eca4900d0803f7b4ae0c15b1e12c2
EzequielG <ezequiel.guaymas07@gmail.com>
2023-05-29 16:27:42 -0300

    Se vio y agrego lo de la clase19 falta ver implementacion de login con mongo por cuenta propia

---
+++ clase18/clase18.js
@@ -0,0 +1,2 @@
+//-------------------------------------------CLASE 18 - Cookies, Sessions & Storages-----------------------------------------------------
+// se baso la clase principalmente en lo referido a cookeis y sessions
---
+++ clase19/clase19.js
@@ -0,0 +1,2 @@
+//-------------------------------------------CLASE 19 - Cookies, Session & Storage II-----------------------------------------------------
+// falta ver implementacion de login con mongo por cortarse grabado
