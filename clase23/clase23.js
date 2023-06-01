b2147f342c121d1ec134e336eed2d78f6e75118e
EzequielG <ezequiel.guaymas07@gmail.com>
2023-06-01 11:50:10 -0300

    Se vio clase 23

---
+++ clase23/clase23.js
@@ -0,0 +1,2 @@
+//-------------------------------------------CLASE 23 - Ruteo avanzado y estrategias avanzadas de autorización-----------------------------------------------------
+//la clase se basa en basicamente como personalizar y crear los ruteos en express aunque no es una practica muy comun
