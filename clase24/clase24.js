902714c13d690c20ca2f60ca9bbd2f0d9feb313a
EzequielG <ezequiel.guaymas07@gmail.com>
2023-06-12 12:22:32 -0300

    Clase24 vista

---
+++ clase24/clase24.js
@@ -0,0 +1,2 @@
+//-------------------------------------------CLASE 24 - Segunda practica integradora-----------------------------------------------------
+//ver como se auntentica con google
--- clase24/src/routes/students.router.js
+++ clase24/src/routes/students.router.js
@@ -1,43 +1,49 @@
-import { Router } from 'express'
+import { Router } from 'express';
 //import StudentsManager from '../Dao/studentsManagerFS.js'
-import StudentsManager from '../Dao/studentsManagerMongo.js'
-import { hashData } from '../utils.js'
-import passport from 'passport'
+import StudentsManager from '../Dao/studentsManagerMongo.js';
+import { hashData } from '../utils.js';
+import passport from 'passport';
 
-const studentsManager = new StudentsManager()
+const studentsManager = new StudentsManager();
 
-const router = Router()
+const router = Router();
 
 router.get('/', async (req, res) => {
-  const students = await studentsManager.getAllStudents()
-  res.json({ message: 'Students', students })
-})
+  const students = await studentsManager.getAllStudents();
+  res.json({ message: 'Students', students });
+});
 
 router.get('/:idStudent', async (req, res) => {
-  const { idStudent } = req.params
-  const student = await studentsManager.getOneById(idStudent)
-  res.json({ message: 'Student', student })
-})
+  const { idStudent } = req.params;
+  const student = await studentsManager.getOneById(idStudent);
+  res.json({ message: 'Student', student });
+});
 
 router.post('/', async (req, res) => {
-  const { password } = req.body
-  const hashPassword = await hashData(password)
-  const newObj = { ...req.body, password: hashPassword }
-  const newStudent = await studentsManager.createOne(newObj)
-  res.json({ message: 'Student created', student: newStudent })
-})
+  const { password } = req.body;
+  const hashPassword = await hashData(password);
+  const newObj = { ...req.body, password: hashPassword };
+  const newStudent = await studentsManager.createOne(newObj);
+  res.json({ message: 'Student created', student: newStudent });
+});
 
 router.post('/localSignup', passport.authenticate('signup'), (req, res) => {
-  res.json({ message: 'User created', user: req.user })
-})
+  res.json({ message: 'User created', user: req.user });
+});
 
-// GOOGEL SIGNUP
+// GOOGLE SIGNUP
 
-router.get('/google',passport.authenticate('googleSignup', { scope: ['profile'] }))
+router.get(
+  '/google',
+  passport.authenticate('googleSignup', { scope: ['profile'] })
+);
 
+router.get(
+  '/googleCallback',
+  passport.authenticate('googleSignup'),
+  (req, res) => {
+    res.send('Logged with google');
+  }
+);
 
-router.get('/googleCallback',passport.authenticate('googleSignup'),(req,res)=>{
-  res.send('Logged with google')
-})
-
-export default router
+export default router;
